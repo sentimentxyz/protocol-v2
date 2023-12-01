b7efc8c4476232c0743c3f0519487a0ce1dcae5a
ruvaag <ruvaag@gmail.com>
2023-12-01 14:54:34 +0530

    feat: add IPositionManager

---
+++ src/IPositionManager.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+interface IPositionManager {
+    function closePosition() external;
+    function createPosition() external;
+
+    function repay(address position, address asset, uint256 amt) external;
+    function borrow(address position, address asset, uint256 amt) external;
+    function deposit(address position, address asset, uint256 amt) external;
+    function withdraw(address position, address asset, uint256 amt) external;
+
+    function liquidate(address position) external;
+    function exec(address position, bytes calldata data) external;
+    function approve(address position, address asset, uint256 amt) external;
+}

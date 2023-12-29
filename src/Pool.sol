9ac55824f88aeab5b6ebda595414609c3cdf35dc
ruvaag <ruvaag@gmail.com>
2023-12-30 05:12:53 +0530

    fix: separate interfaces dir

--- src/Pool.sol
+++ src/Pool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import {IRateModel} from "./IRateModel.sol";
+import {IRateModel} from "./interfaces/IRateModel.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 

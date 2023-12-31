4d24a0ae368ea91b04bbc4eeb5c09c960a75e4ef
ruvaag <ruvaag@gmail.com>
2024-01-01 01:49:57 +0530

    feat: view functions for IPosition

--- src/interfaces/IPosition.sol
+++ src/interfaces/IPosition.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.23;
 
 interface IPosistion {
-    function exec(address target, bytes calldata data) external;
+    // view functions
+    function getAssets() external view returns (address[] memory);
+    function getDebtPools() external view returns (address[] memory);
 
     function repay(address asset, uint256 amt) external;
     function borrow(address asset, uint256 amt) external;
     function deposit(address asset, uint256 amt) external;
     function withdraw(address asset, uint256 amt) external;
+    function exec(address target, bytes calldata data) external;
 }

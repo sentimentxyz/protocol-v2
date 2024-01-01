0327b4f2557c133983ed816467d8e34a23e184e2
ruvaag <ruvaag@gmail.com>
2024-01-01 07:06:33 +0530

    feat: single debt pool position, generic iterablemap

--- src/Position.sol
+++ src/Position.sol
@@ -5,9 +5,11 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import {IPool} from "./interfaces/IPool.sol";
+import {IterableMap} from "./lib/IterableMap.sol";
 
 contract Position {
     using SafeERC20 for IERC20;
+    using IterableMap for IterableMap.IterableMapStorage;
 
     // single debt pool; multiple position assets
     uint8 public constant TYPE = 0x1;
@@ -16,8 +18,7 @@ contract Position {
     address public debtPool;
     address public positionManager;
 
-    address[] public assets;
-    mapping(address => uint256) balanceOf;
+    IterableMap.IterableMapStorage internal assets;
 
     error InvalidOperation();
     error PositionManagerOnly();
@@ -27,15 +28,13 @@ contract Position {
         _;
     }
 
+    /// @dev assume that funds are sent in the same txn before deposit is called
     function deposit(address asset, uint256 amt) external onlyPositionManager {
-        if (balanceOf[asset] == 0) assets.push(asset);
-        balanceOf[asset] = amt;
+        assets.set(asset, amt);
     }
 
     function withdraw(address asset, uint256 amt) external onlyPositionManager {
-        if ((balanceOf[asset] -= amt) == 0) {
-            // TODO remove from assets
-        }
+        assets.set(asset, assets.get(asset) - amt);
         IERC20(asset).safeTransfer(owner, amt);
     }
 
@@ -47,10 +46,11 @@ contract Position {
         }
     }
 
-    function repay(address pool, uint256 _amt) external onlyPositionManager {
-        if (pool != debtPool) revert InvalidOperation();
-        uint256 amt = (_amt == type(uint256).max) ? IPool(pool).getBorrowsOf(address(this)) : _amt;
-        IERC20(IPool(debtPool).asset()).safeTransfer(pool, amt);
+    function repay(address _pool, uint256 _amt) external onlyPositionManager {
+        if (_pool != debtPool) revert InvalidOperation();
+        IPool pool = IPool(_pool);
+        uint256 amt = (_amt == type(uint256).max) ? pool.getBorrowsOf(address(this)) : _amt;
+        IERC20(IPool(debtPool).asset()).safeTransfer(address(pool), amt);
         if (IPool(pool).repay(address(this), amt) == 0) {
             debtPool = address(0);
         }
@@ -59,5 +59,15 @@ contract Position {
     function exec(address target, uint256 amt, bytes calldata data) external onlyPositionManager {
         (bool success,) = target.call{value: amt}(data);
         if (!success) revert InvalidOperation();
+    }
+
+    function getAssets() external view returns (address[] memory) {
+        return assets.getKeys();
+    }
+
+    function getDebtPools() external view returns (address[] memory) {
+        address[] memory debtPools = new address[](1);
+        debtPools[0] = debtPool;
+        return debtPools;
     }
 }

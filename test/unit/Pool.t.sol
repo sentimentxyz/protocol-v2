655553bf255eb51f251f1e0bb4e337bb375423c7
N <n@push0.dev>
2024-01-26 02:02:06 -0500

    big num

--- test/unit/Pool.t.sol
+++ test/unit/Pool.t.sol
@@ -11,6 +11,8 @@ contract PoolTest is BaseTest {
     MintableToken public mockToken;
     MockRateModel public mockRateModel;
 
+    uint256 constant BIG_NUMBER = 100000000000000000000000e18;
+
     function setUp() public override {
         mockToken = new MintableToken();
         pool = new Pool(address(mockToken), "mock pool", "mp");
@@ -26,7 +28,7 @@ contract PoolTest is BaseTest {
     function testExpectedBorrowSharesMinted(uint256 debt) public {
         // play nicely with the test setup / rounding
         vm.assume(debt% 4 == 0);
-        vm.assume(debt < 100000000000000000000000e18);
+        vm.assume(debt < BIG_NUMBER);
         vm.assume(debt > 1 gwei);
 
         /// On the first mint we should have 1:1 shares
@@ -54,7 +56,7 @@ contract PoolTest is BaseTest {
 
     function testExpectedBorrowSharesBurned(uint256 debt) public {
         vm.assume(debt% 2 == 0);
-        vm.assume(debt < 100000000000000000000000e18);
+        vm.assume(debt < BIG_NUMBER);
         vm.assume(debt > 0);
 
         /// On the first mint we should have 1:1 shares

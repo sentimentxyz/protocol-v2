375253b3b34ef52b5f2309c3508cbf0a11fdcf97
ruvaag <ruvaag@gmail.com>
2024-02-09 16:48:07 +0530

    feat: lens contracts (#59)
    * feat: impl getInterestRate
    
    * feat: portfolio lens
    
    * feat: create lens dir
    
    * feat: superpool lens

--- src/LinearRateModel.sol
+++ src/LinearRateModel.sol
@@ -16,7 +16,7 @@ contract LinearRateModel is IRateModel {
     uint256 public immutable MIN_RATE; // 18 decimal scaled APR
     uint256 public immutable MAX_RATE; // 18 decimal scaled APR
     uint256 immutable RATE_DIFF; // MAX_RATE - MIN_RATE
-    uint256 constant SECONDS_PER_YEAR = 31_557_600e18; // 1 year = 365.25 days
+    uint256 constant SECONDS_PER_YEAR = 31_557_600; // 1 year = 365.25 days
 
     /*//////////////////////////////////////////////////////////////
                               Initialize
@@ -30,22 +30,27 @@ contract LinearRateModel is IRateModel {
     }
 
     /*//////////////////////////////////////////////////////////////
-                        Public View Functions
+                            View Functions
     //////////////////////////////////////////////////////////////*/
+
+    function getInterestRate(uint256 borrows, uint256 idleAmt) public view returns (uint256) {
+        // util = borrows / (borrows + idleAmt)
+        uint256 util = borrows.mulDiv(1e18, borrows + idleAmt, Math.Rounding.Ceil);
+
+        // interest rate = MIN_RATE + util * (MAX_RATE - MIN_RATE)
+        return MIN_RATE + util.mulDiv(RATE_DIFF, 1e18, Math.Rounding.Ceil);
+    }
 
     /// @notice calculates the interest accrued since the last update
     /// @param lastUpdated the timestamp of the last update
     /// @param borrows the total amount of borrows
     /// @return interest accrued since the last update
     function interestAccrued(uint256 lastUpdated, uint256 borrows, uint256 idleAmt) external view returns (uint256) {
-        // util = borrows / (borrows + idleAmt)
-        uint256 util = borrows.mulDiv(1e18, borrows + idleAmt, Math.Rounding.Ceil);
-
-        // interest rate = MIN_RATE + util * (MAX_RATE - MIN_RATE)
-        uint256 rate = MIN_RATE + util.mulDiv(RATE_DIFF, 1e18, Math.Rounding.Ceil);
-
         // rateFactor = time delta * apr / secs_per_year
-        uint256 rateFactor = ((block.timestamp - lastUpdated) * 1e18).mulDiv(rate, SECONDS_PER_YEAR, Math.Rounding.Ceil);
+        // rate is scaled but time delta and seconds_per_year are not scaled, to preserve precision
+        uint256 rateFactor = ((block.timestamp - lastUpdated)).mulDiv(
+            getInterestRate(borrows, idleAmt), SECONDS_PER_YEAR, Math.Rounding.Ceil
+        );
 
         // interest accrued = borrows * rateFactor
         return borrows.mulDiv(rateFactor, 1e18, Math.Rounding.Ceil);
---
+++ src/lens/SuperPoolLens.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// types
+import {Pool} from "../Pool.sol";
+import {SuperPool} from "../SuperPool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+// libraries
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract SuperPoolLens {
+    using Math for uint256;
+
+    /*//////////////////////////////////////////////////////////////
+                             Data Structs
+    //////////////////////////////////////////////////////////////*/
+
+    struct SuperPoolData {
+        string name;
+        address asset;
+        uint256 idleAssets;
+        uint256 totalAssets;
+        uint256 interestRate;
+        PoolDepositData[] deposits;
+    }
+
+    struct PoolDepositData {
+        address pool;
+        address asset;
+        uint256 amount;
+        uint256 interestRate;
+    }
+
+    struct UserDepositData {
+        uint256 interestRate;
+        uint256 totalDeposits;
+        SuperPoolDepositData[] deposits;
+    }
+
+    struct SuperPoolDepositData {
+        address superPool;
+        address asset;
+        uint256 amount;
+        uint256 interestRate;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            SuperPool View
+    //////////////////////////////////////////////////////////////*/
+
+    function getSuperPoolData(address _superPool) external view returns (SuperPoolData memory) {
+        SuperPool superPool = SuperPool(_superPool);
+        address[] memory pools = superPool.pools();
+
+        PoolDepositData[] memory deposits = new PoolDepositData[](pools.length);
+        for (uint256 i; i < pools.length; ++i) {
+            deposits[i] = getPoolDepositData(_superPool, pools[i]);
+        }
+
+        address asset = superPool.asset();
+
+        return SuperPoolData({
+            asset: asset,
+            deposits: deposits,
+            name: superPool.name(),
+            totalAssets: superPool.totalAssets(),
+            idleAssets: IERC20(asset).balanceOf(_superPool),
+            interestRate: getSuperPoolInterestRate(_superPool)
+        });
+    }
+
+    function getPoolDepositData(address superPool, address _pool) public view returns (PoolDepositData memory) {
+        Pool pool = Pool(_pool);
+        address asset = pool.asset();
+
+        return PoolDepositData({
+            pool: _pool,
+            asset: asset,
+            interestRate: getPoolInterestRate(_pool),
+            amount: IERC4626(_pool).previewRedeem(IERC20(asset).balanceOf(superPool))
+        });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              User View
+    //////////////////////////////////////////////////////////////*/
+
+    function getUserDepositData(address user, address[] calldata superPools)
+        public
+        view
+        returns (UserDepositData memory)
+    {
+        SuperPoolDepositData[] memory deposits = new SuperPoolDepositData[](superPools.length);
+
+        uint256 totalDeposits;
+        uint256 weightedDeposit;
+
+        for (uint256 i; i < superPools.length; ++i) {
+            deposits[i] = getSuperPoolDepositData(user, superPools[i]);
+
+            totalDeposits += deposits[i].amount;
+            weightedDeposit += (deposits[i].amount).mulDiv(deposits[i].interestRate, 1e18);
+        }
+        return UserDepositData({
+            deposits: deposits,
+            totalDeposits: totalDeposits,
+            interestRate: weightedDeposit.mulDiv(1e18, totalDeposits)
+        });
+    }
+
+    function getSuperPoolDepositData(address user, address _superPool)
+        public
+        view
+        returns (SuperPoolDepositData memory)
+    {
+        SuperPool superPool = SuperPool(_superPool);
+        address asset = superPool.asset();
+
+        return SuperPoolDepositData({
+            superPool: _superPool,
+            asset: asset,
+            interestRate: getSuperPoolInterestRate(_superPool),
+            amount: superPool.previewRedeem(IERC20(asset).balanceOf(user))
+        });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         Interest Rates View
+    //////////////////////////////////////////////////////////////*/
+
+    function getPoolInterestRate(address _pool) public view returns (uint256) {
+        Pool pool = Pool(_pool);
+        uint256 borrows = pool.getBorrows();
+        uint256 idleAmt = IERC20(pool.asset()).balanceOf(_pool);
+
+        return pool.rateModel().getInterestRate(borrows, idleAmt);
+    }
+
+    function getSuperPoolInterestRate(address _superPool) public view returns (uint256) {
+        SuperPool superPool = SuperPool(_superPool);
+        address asset = superPool.asset();
+        uint256 totalAssets = superPool.totalAssets();
+
+        uint256 weightedAssets;
+        address[] memory pools = superPool.pools();
+        for (uint256 i; i < pools.length; ++i) {
+            uint256 assets = IERC4626(pools[i]).previewRedeem(IERC20(asset).balanceOf(_superPool));
+            weightedAssets += assets.mulDiv(getPoolInterestRate(pools[i]), 1e18);
+        }
+
+        return weightedAssets.mulDiv(1e18, totalAssets);
+    }
+}

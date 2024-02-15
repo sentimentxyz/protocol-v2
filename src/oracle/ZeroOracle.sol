036d2bd4ffe84f48c9290b9020203b76f367736b
ruvaag <ruvaag@gmail.com>
2024-02-15 10:21:53 +0530

    feat: fixed price oracles (#76)
    * feat: add zero oracle
    
    * feat: fixed price oracle

---
+++ src/oracle/ZeroOracle.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// types
+import {IOracle} from "../interfaces/IOracle.sol";
+
+contract ZeroOracle {
+    function getValueInEth(address, uint256) external pure returns (uint256) {
+        return 0;
+    }
+}

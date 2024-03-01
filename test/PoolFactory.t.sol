c86c6f56f9596af2ba1ad5acf2c63b3f9febd981
ruvaag <ruvaag@gmail.com>
2024-03-01 17:56:24 +0530

    fix: all test file names end with .t.sol (#97)

--- test/PoolFactory.t.sol
+++ test/PoolFactory.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {BaseTest} from "./BaseTest.sol";
+import {BaseTest} from "./BaseTest.t.sol";
 import {PoolDeployParams} from "src/PoolFactory.sol";
 
 contract PoolFactoryTest is BaseTest {

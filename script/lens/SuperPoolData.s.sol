552502d2e281282400b052c5fcb663b039b06265
ruvaag <ruvaag@gmail.com>
2024-03-06 21:33:15 +0530

    fix: handle zero assets case for superpool lens

---
+++ script/lens/SuperPoolData.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../BaseScript.s.sol";
+import {SuperPoolLens} from "src/lens/SuperPoolLens.sol";
+
+contract SuperPoolData is BaseScript {
+    function run() public {
+        SuperPoolLens superPoolLens = SuperPoolLens(0x120659e930795E87a860ee853066004bF9E44479);
+        superPoolLens.getSuperPoolData(0xD7Fc4E31313fE889764313341d08229047db2b44);
+    }
+}

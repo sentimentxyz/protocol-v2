f72e857ba1ea3639038c0897176c2cca3bf5f518
ruvaag <ruvaag@gmail.com>
2024-03-21 09:52:52 +0530

    fix: hash new position salt with owner

--- src/lens/PortfolioLens.sol
+++ src/lens/PortfolioLens.sol
@@ -113,7 +113,9 @@ contract PortfolioLens {
         return debtData;
     }
 
-    function predictAddress(uint256 positionType, bytes32 salt) external view returns (address) {
+    function predictAddress(address owner, uint256 positionType, bytes32 salt) external view returns (address) {
+        salt = keccak256(abi.encodePacked(owner, salt));
+
         bytes memory creationCode =
             abi.encodePacked(type(BeaconProxy).creationCode, abi.encode(POSITION_MANAGER.beaconFor(positionType), ""));
 

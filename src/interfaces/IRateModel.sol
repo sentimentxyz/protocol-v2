1a6f7ba32c36d3c427da0a8cd7b0cd08724debe2
ruvaag <ruvaag@gmail.com>
2024-05-24 05:54:18 +0530

    docs: interfaces

--- src/interfaces/IRateModel.sol
+++ src/interfaces/IRateModel.sol
@@ -1,14 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+/*//////////////////////////////////////////////////////////////
+                            IRateModel
+//////////////////////////////////////////////////////////////*/
+
+/// @title IRateModel
+/// @notice Common interface for all rate model implementations
 interface IRateModel {
-    /*//////////////////////////////////////////////////////////////
-                            View Functions
-    //////////////////////////////////////////////////////////////*/
-    function interestAccrued(uint256 lastUpdated, uint256 borrows, uint256 idleAssetAmt)
+    /// @notice Compute the amount of interest accrued since the last interest update
+    /// @param lastUpdated Timestamp of the last interest update
+    /// @param totalBorrows Total amount of assets borrowed from the pool
+    /// @param idleAssetAmt Total amount of idle liquidity in the pool
+    /// @return interestAccrued Amount of interest accrued since the last interest update
+    ///         denominated in terms of the given asset
+    function getInterestAccrued(uint256 lastUpdated, uint256 totalBorrows, uint256 idleAssetAmt)
         external
         view
-        returns (uint256 interest);
+        returns (uint256 interestAccrued);
 
-    function getInterestRate(uint256 borrows, uint256 idleAssetAmt) external view returns (uint256 interestRate);
+    /// @notice Fetch the instantaneous borrow interest rate for a given pool state
+    /// @param totalBorrows Total amount of assets borrowed from the pool
+    /// @param idleAssetAmt Total amount of idle liquidity in the pool
+    /// @return interestRate Instantaneous interest rate for the given pool state, scaled by 18 decimals
+    function getInterestRate(uint256 totalBorrows, uint256 idleAssetAmt) external view returns (uint256 interestRate);
 }

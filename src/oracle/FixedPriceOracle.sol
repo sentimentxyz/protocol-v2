74b93a0b693fbf92c100dac66ebebb40a6b8d2cf
ruvaag <ruvaag@gmail.com>
2024-05-24 05:56:02 +0530

    docs: oracles

--- src/oracle/FixedPriceOracle.sol
+++ src/oracle/FixedPriceOracle.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-// types
 import {IOracle} from "../interfaces/IOracle.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-// libraries
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract FixedPriceOracle {
     using Math for uint256;

04f2a0ae96b04f88c18e597d7960d6d96ed8faab
ruvaag <ruvaag@gmail.com>
2024-05-26 07:56:58 +0530

    feat: registry natspec

--- src/Registry.sol
+++ src/Registry.sol
@@ -1,19 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+/*//////////////////////////////////////////////////////////////
+                            Registry
+//////////////////////////////////////////////////////////////*/
+
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Registry
 contract Registry is Ownable {
+    /// @notice Registry address for a given key hash has been updated
     event AddressSet(bytes32 indexed key, address addr);
 
+    /// @notice Fetch registry address for a given key hash
     mapping(bytes32 key => address addr) public addressFor;
 
     constructor() Ownable(msg.sender) { }
 
+    /// @notice Update registry address for a given key hash
+    /// @param key Registry key hash
+    /// @param addr Updated registry address for the key hash
     function setAddress(bytes32 key, address addr) external onlyOwner {
         addressFor[key] = addr;
-
         emit AddressSet(key, addr);
     }
 }

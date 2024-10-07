80b0d53b6119de9479c763314ee6763dac8e8cda
ruvaag <ruvaag@gmail.com>
2024-10-07 19:22:26 +0530

    fix: redstone oracle timestamp and decimals

--- src/oracle/RedstoneOracle.sol
+++ src/oracle/RedstoneOracle.sol
@@ -12,15 +12,14 @@ import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 contract RedstoneOracle is PrimaryProdDataServiceConsumerBase, IOracle {
     using Math for uint256;
 
-    uint256 internal constant THREE_MINUTES = 60 * 3;
     address internal constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
-    // stale price threshold, prices older than this period are considered stale
-    // the oracle can misreport stale prices for feeds with longer hearbeats
     uint256 public constant STALE_PRICE_THRESHOLD = 3600; // 1 hour
 
     address public immutable ASSET;
     uint256 public immutable ASSET_DECIMALS;
+
+    uint256 public immutable ETH_FEED_DECIMALS;
+    uint256 public immutable ASSET_FEED_DECIMALS;
 
     bytes32 public immutable ETH_FEED_ID;
     bytes32 public immutable ASSET_FEED_ID;
@@ -35,33 +34,50 @@ contract RedstoneOracle is PrimaryProdDataServiceConsumerBase, IOracle {
 
     error RedstoneOracle_ZeroPrice(address asset);
     error RedstoneOracle_StalePrice(address asset);
+    error RedstoneOracle_InvalidTimestamp(uint256 timestamp);
 
-    constructor(address asset, bytes32 assetFeedId, bytes32 ethFeedId) {
+    constructor(
+        address asset,
+        bytes32 assetFeedId,
+        bytes32 ethFeedId,
+        uint256 assetFeedDecimals,
+        uint256 ethFeedDecimals
+    ) {
         ASSET = asset;
         ASSET_DECIMALS = IERC20Metadata(asset).decimals();
 
         ASSET_FEED_ID = assetFeedId;
         ETH_FEED_ID = ethFeedId;
 
+        ASSET_FEED_DECIMALS = assetFeedDecimals;
+        ETH_FEED_DECIMALS = ethFeedDecimals;
+
         dataFeedIds[0] = assetFeedId;
         dataFeedIds[1] = ethFeedId;
     }
 
     function updatePrice() external {
-        // values[0] -> price of ASSET/USD
-        // values[1] -> price of ETH/USD
-        // values are scaled to 8 decimals
-        uint256[] memory values = getOracleNumericValuesFromTxMsg(dataFeedIds);
+        // fetch ASSET/USD and ETH/USD price with package timestamp
+        // values[0] -> ASSET/USD price with ASSET_FEED_DECIMALS decimals
+        // values[1] -> ETH/USD price with ETH_FEED_DECIMALS decimals
+        // timestamp -> data package timestamp in milliseconds
+        (uint256[] memory values, uint256 timestamp) = getOracleNumericValuesAndTimestampFromTxMsg(dataFeedIds);
 
+        // non-zero price checks
         if (values[0] == 0) revert RedstoneOracle_ZeroPrice(ASSET);
         if (values[1] == 0) revert RedstoneOracle_ZeroPrice(ETH);
-        assetUsdPrice = values[0];
-        ethUsdPrice = values[1];
 
-        // RedstoneDefaultLibs.sol enforces that prices are not older than 3 mins. since it is not
-        // possible to retrieve timestamps for individual prices being passed, we consider the worst
-        // case and assume both prices are 3 mins old
-        priceTimestamp = block.timestamp - THREE_MINUTES;
+        // scale ASSET/USD price to 18 decimals
+        if (ASSET_FEED_DECIMALS <= 18) assetUsdPrice = values[0] * (10 ** (18 - ASSET_FEED_DECIMALS));
+        else assetUsdPrice = values[0] / (10 ** (ASSET_FEED_DECIMALS - 18));
+
+        // scale ETH/USD price to 18 decimals
+        if (ETH_FEED_DECIMALS <= 18) ethUsdPrice = values[1] * (10 ** (18 - ETH_FEED_DECIMALS));
+        else ethUsdPrice = values[1] / (10 ** (ETH_FEED_DECIMALS - 18));
+
+        // update price timestamp with checks
+        if (priceTimestamp > timestamp) revert RedstoneOracle_InvalidTimestamp(timestamp);
+        priceTimestamp = timestamp;
     }
 
     function getValueInEth(address, uint256 amt) external view returns (uint256) {

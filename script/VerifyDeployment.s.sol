e2ec1f6ecbf305787692bb4e6b3e0e734a2b4c09
sriyantra <85423375+sriyantra@users.noreply.github.com>
2025-03-07 09:49:49 -0800

    fix: update script

--- script/VerifyDeployment.s.sol
+++ script/VerifyDeployment.s.sol
@@ -67,19 +67,29 @@ contract VerifyDeployment is BaseScript {
         0x881469d14b8443f6c918bdd0a641e9d7cae2592dc28a4f922a2c4d7ca3d19c77;
 
     // Assets
-    address public constant mockwHYPE = 0xB3fB66C10fD75E7ceB7E491d8dF505De0d91d340;
-    address public constant mockUSDC = 0xdeC702aa5a18129Bd410961215674A7A130A12e5;
-    address public constant wHYPE = 0x5555555555555555555555555555555555555555;
-    address public constant wstHYPE = 0x94e8396e0869c9F2200760aF0621aFd240E1CF38;
+    address public constant borrowAsset = 0x5555555555555555555555555555555555555555;
+    address public constant collateralAsset = 0x94e8396e0869c9F2200760aF0621aFd240E1CF38;
+
+    bytes32 constant SALT = "INITIAL_TEST_SALT";
 
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        // Set live contract addresses
+        superPool = SuperPool(0x17D9bA6c4276A5A679221B7128Ad3301d2b857B1);
+        pool = Pool(0xE5B81a2bdaE122EE8E538CF866d721F09539556F);
+        riskEngine = RiskEngine(0x5f7e170Be9ac684fF221b55B956d95b10eaBA3C8);
+        portfolioLens = PortfolioLens(0xF3487f4731f63B9AD94aAEa1F8A97a38Ec64c2E9);
+        positionManager = PositionManager(0xE709523Bf6902b757B1A741187b5c10F2e24e463);
+
+        oracle1 = AggV3Oracle(0x79479c3d10b7fF49D6c18A5ADC601c86472D4767);
+        oracle2 = AggV3Oracle(0x712047cC3e4b0023Fccc09Ae412648CF23C65ed3);
+
         _run();
     }
 
     function _run() internal {
         // SuperPool
-        superPool = SuperPool(0x17D9bA6c4276A5A679221B7128Ad3301d2b857B1);
         console2.log("superpool: ", superPool.name());
         console2.log("asset: ", superPool.asset());
         console2.log("owner: ", superPool.owner());
@@ -90,7 +100,6 @@ contract VerifyDeployment is BaseScript {
         console2.log("superPoolCap: ", superPool.superPoolCap());
 
         // Pool
-        pool = Pool(0xE5B81a2bdaE122EE8E538CF866d721F09539556F);
         console2.log("pool: ");
         console2.log("proxy owner: ", pool.owner());
         console2.log("pool owner: ", pool.ownerOf(poolId));
@@ -103,41 +112,44 @@ contract VerifyDeployment is BaseScript {
         console2.log("borrowCap: ", pool.getBorrowCapFor(poolId));
 
         //RiskEngine
-        riskEngine = RiskEngine(0x5f7e170Be9ac684fF221b55B956d95b10eaBA3C8);
         console2.log("RiskEngine: ", address(riskEngine));
-        console2.log("asset1 ltv: ", riskEngine.ltvFor(poolId, wstHYPE));
-        console2.log("asset2 ltv: ", riskEngine.ltvFor(poolId, wHYPE));
+        console2.log("collateralAsset ltv: ", riskEngine.ltvFor(poolId, collateralAsset));
+        console2.log("borrowAsset ltv: ", riskEngine.ltvFor(poolId, borrowAsset));
 
         // Oracles
-        oracle1 = AggV3Oracle(0x79479c3d10b7fF49D6c18A5ADC601c86472D4767);
-        oracle2 = AggV3Oracle(0x712047cC3e4b0023Fccc09Ae412648CF23C65ed3);
-        console2.log("oracle1 price: ", oracle1.getValueInEth(wstHYPE, 1e18));
-        console2.log("oracle2 price: ", oracle2.getValueInEth(wHYPE, 1e18));
+        console2.log("collateralAsset price: ", oracle1.getValueInEth(collateralAsset, 1e18));
+        console2.log("borrowAsset price: ", oracle2.getValueInEth(borrowAsset, 1e18));
 
-        IERC20(wHYPE).approve(address(superPool), 1e17);
+        // PositionManager
+        console2.log("borrowAsset toggled: ", positionManager.isKnownAsset(borrowAsset));
+        console2.log("collateralAsset toggled: ", positionManager.isKnownAsset(collateralAsset));
+
+        /// Deposit/borrow/repay scenarios
+
+        // Deposit liquidity
+        /*IERC20(borrowAsset).approve(address(superPool), 1e17);
         superPool.deposit(1e17, 0xB290f2F3FAd4E540D0550985951Cdad2711ac34A);
-        portfolioLens = PortfolioLens(0xF3487f4731f63B9AD94aAEa1F8A97a38Ec64c2E9);
-        positionManager = PositionManager(0xE709523Bf6902b757B1A741187b5c10F2e24e463);
         (address position, bool available) =
-            portfolioLens.predictAddress(0xB290f2F3FAd4E540D0550985951Cdad2711ac34A, "TESTSALT");
+            portfolioLens.predictAddress(0xB290f2F3FAd4E540D0550985951Cdad2711ac34A, SALT);
         available;
 
+        // Open new position, deposit, and borrow
         Action[] memory actions = new Action[](5);
-        /*
-        actions[0] = ActionUtils.newPosition(0xB290f2F3FAd4E540D0550985951Cdad2711ac34A, "TESTSALT");
-        actions[1] = ActionUtils.deposit(address(wstHYPE), 2e17);
-        actions[2] = ActionUtils.addToken(address(wstHYPE));
+        actions[0] = ActionUtils.newPosition(0xB290f2F3FAd4E540D0550985951Cdad2711ac34A, SALT);
+        actions[1] = ActionUtils.deposit(address(collateralAsset), 2e17);
+        actions[2] = ActionUtils.addToken(address(collateralAsset));
         actions[3] = ActionUtils.borrow(poolId, 1e16);
-        actions[4] = ActionUtils.transfer(0xB290f2F3FAd4E540D0550985951Cdad2711ac34A, address(wHYPE), 1e16);
-
-        IERC20(wstHYPE).approve(address(positionManager), 2e17);
-        positionManager.processBatch(position, actions);*/
-
-        IERC20(wHYPE).approve(address(positionManager), 1e10);
-
-        actions = new Action[](2);
-        actions[0] = ActionUtils.deposit(address(wHYPE), 1e10);
-        actions[1] = ActionUtils.repay(poolId, 1e10);
+        actions[4] = ActionUtils.transfer(0xB290f2F3FAd4E540D0550985951Cdad2711ac34A, address(borrowAsset), 1e16);
+        
+        IERC20(collateralAsset).approve(address(positionManager), 2e17);
         positionManager.processBatch(position, actions);
+
+        // Partially repay borrowed asset
+        actions = new Action[](2);
+        actions[0] = ActionUtils.deposit(address(borrowAsset), 1e10);
+        actions[1] = ActionUtils.repay(poolId, 1e10);
+        
+        IERC20(borrowAsset).approve(address(positionManager), 1e10);
+        positionManager.processBatch(position, actions);*/
     }
 }

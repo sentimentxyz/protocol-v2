e196ff41f30fd2750d7644a95bbf39a40e37d348
sriyantra <85423375+sriyantra@users.noreply.github.com>
2025-03-13 15:47:51 -0700

    fix: add feedDecimals and test

--- src/oracle/MetaOracle.sol
+++ src/oracle/MetaOracle.sol
@@ -15,18 +15,32 @@ contract MetaOracle is IOracle {
 
     uint256 public immutable WAD = 1e18;
     uint256 public immutable ASSET_DECIMALS;
+    uint256 public immutable FEED_DECIMALS_A;
+    uint256 public immutable FEED_DECIMALS_B;
+    uint256 public immutable FEED_DECIMALS_C;
 
-    constructor(IOracle a, IOracle b, IOracle c, uint256 assetDecimals) {
+    constructor(
+        IOracle a,
+        IOracle b,
+        IOracle c,
+        uint256 assetDecimals,
+        uint256 feedDecimalsA,
+        uint256 feedDecimalsB,
+        uint256 feedDecimalsC
+    ) {
         A = a;
         B = b;
         C = c;
         ASSET_DECIMALS = assetDecimals;
+        FEED_DECIMALS_A = feedDecimalsA;
+        FEED_DECIMALS_B = feedDecimalsB;
+        FEED_DECIMALS_C = feedDecimalsC;
     }
 
     function getValueInEth(address asset, uint256 amt) external view returns (uint256 value) {
-        uint256 valueA = address(A) == address(0) ? WAD : A.getValueInEth(asset, WAD);
-        uint256 valueB = address(B) == address(0) ? WAD : B.getValueInEth(asset, WAD);
-        uint256 valueC = address(C) == address(0) ? WAD : C.getValueInEth(asset, WAD);
+        uint256 valueA = address(A) == address(0) ? WAD : A.getValueInEth(asset, 10 ** FEED_DECIMALS_A);
+        uint256 valueB = address(B) == address(0) ? WAD : B.getValueInEth(asset, 10 ** FEED_DECIMALS_B);
+        uint256 valueC = address(C) == address(0) ? WAD : C.getValueInEth(asset, 10 ** FEED_DECIMALS_C);
 
         value = amt.mulDiv(valueA.mulDiv(valueB, valueC), 10 ** ASSET_DECIMALS);
     }
--- test/oracle/MetaOracleTest.t.sol
+++ test/oracle/MetaOracleTest.t.sol
@@ -33,10 +33,56 @@ contract MetaOracleTest is BaseTest {
     function testMetaOracle() public {
         aggV3OracleA = AggV3Oracle(0x712047cC3e4b0023Fccc09Ae412648CF23C65ed3); // wHype
         aggV3OracleB = AggV3Oracle(0xb4AEd75ec810729Ee0fD375Ff3ADe8eD03d1eA96); // wstHype/wHype RR, returns 18 decimals
+        metaOracle = new MetaOracle(aggV3OracleA, aggV3OracleB, IOracle(address(0)), 18, 18, 18, 18);
 
-        metaOracle = new MetaOracle(aggV3OracleA, aggV3OracleB, IOracle(address(0)), 18);
         console2.log("aggV3OracleA price: ", aggV3OracleA.getValueInEth(address(0), 1e18));
         console2.log("aggV3OracleB price: ", aggV3OracleB.getValueInEth(address(0), 1e18));
         console2.log("metaOracle price: ", metaOracle.getValueInEth(address(0), 1e18));
+    }
+
+    function testVariableDecimals() public {
+        vm.warp(block.timestamp + 1 days);
+
+        mockEthFeed = new MockV3Aggregator(18, 1e18);
+        mockUsdFeed = new MockV3Aggregator(6, 1e6);
+        mockEthPriceFeed = new MockV3Aggregator(8, 2000e8);
+
+        aggV3OracleA = new AggV3Oracle(
+            address(asset1),
+            address(mockEthFeed),
+            18,
+            18,
+            true,
+            STALE_PRICE_THRESHOLD,
+            false,
+            address(0),
+            address(0),
+            0,
+            false,
+            0
+        );
+
+        aggV3OracleB = new AggV3Oracle(
+            address(asset2),
+            address(mockUsdFeed),
+            6,
+            6,
+            true,
+            STALE_PRICE_THRESHOLD,
+            true,
+            ETH,
+            address(mockEthPriceFeed),
+            8,
+            true,
+            STALE_PRICE_THRESHOLD
+        );
+        uint256 assetDecimals = 18;
+        metaOracle = new MetaOracle(aggV3OracleA, aggV3OracleB, IOracle(address(0)), assetDecimals, 18, 6, 18);
+
+        assertEq(
+            metaOracle.getValueInEth(address(0), 1e18),
+            aggV3OracleA.getValueInEth(address(0), 1e18) * aggV3OracleB.getValueInEth(address(0), 1e6)
+                / (10 ** assetDecimals)
+        );
     }
 }
